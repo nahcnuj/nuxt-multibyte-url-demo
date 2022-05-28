1976333a850b3d6ea8ac8613e71f3a45f1d6f991
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-05-28 12:29:55 +0900

    create and apply a patch to fix

--- dist/_nuxt/static/1653674299/hoge/日本語/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1653674299",layout:"default",error:a,serverRendered:true,routePath:"\u002Fhoge\u002F%E6%97%A5%E6%9C%AC%E8%AA%9E",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ dist/_nuxt/static/1653708545/hoge/日本語/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1653708545",layout:"default",error:a,serverRendered:true,routePath:"\u002Fhoge\u002F%E6%97%A5%E6%9C%AC%E8%AA%9E",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
